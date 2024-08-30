1ed95d1fe0e8d246ac18aaa07fe466d919e2c78c
Mahder Tesfaye Abebe <mahder.tesfaye@aait.edu.et>
2024-08-30 03:35:51 +0300

    done upto the form part but not including the form

--- reactlab/src/components/Card.js
+++ reactlab/src/components/Card.js
@@ -1,4 +1,4 @@
-import { Heading, HStack, Image, Text, VStack } from "@chakra-ui/react";
+import { Center, Heading, HStack, Image, Text, VStack } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
@@ -7,7 +7,25 @@ const Card = ({ title, description, imageSrc }) => {
   // Implement the UI for the Card component according to the instructions.
   // You should be able to implement the component with the elements imported above.
   // Feel free to import other UI components from Chakra UI if you wish to.
-  return null;
+  return(
+    <div>
+      
+      <VStack  borderRadius="0.7rem" bg="white" textColor="black" alignItems="flex-start">
+      <img style={{borderRadius:"0.7rem"}} src={imageSrc} alt="product image"/>
+      <div style={{padding:"0.6rem"}}>
+      <VStack alignItems="flex-start">
+      <h1 style={{fontWeight:"bold"}}>{title}</h1>
+      <p>{description}</p>
+      <p> See More<FontAwesomeIcon icon={faArrowRight} size="1x"/></p>
+      </VStack>
+      
+      </div>
+                
+      
+      
+      </VStack>
+    </div>
+  )
 };
 
 export default Card;
--- reactlab/src/components/LandingSection.js
+++ reactlab/src/components/LandingSection.js
@@ -2,8 +2,8 @@ import React from "react";
 import { Avatar, Heading, VStack } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
 
-const greeting = "Hello, I am Pete!";
-const bio1 = "A frontend developer";
+const greeting = "Hello, I am Mahder!";
+const bio1 = "An Aspiring frontend developer at Google";
 const bio2 = "specialised in React";
 
 // Implement the UI for the LandingSection component according to the instructions.
@@ -15,6 +15,14 @@ const LandingSection = () => (
     isDarkBackground
     backgroundColor="#2A4365"
   >
+    <div>
+    <img style={{borderRadius:"14em"}} src="https://i.pravatar.cc/150?img=7" alt="My photo"/>
+    <p>{greeting}</p>
+    </div>
+   <div style={{textAlign:"center", fontSize:"1.4rem" }}>
+   <p>{bio1}</p>
+   <p>{bio2}</p>
+   </div>
 
   </FullScreenSection>
 );
